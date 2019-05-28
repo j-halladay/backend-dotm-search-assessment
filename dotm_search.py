9993e254c9cb6e6464da718841686a885601b294
j_halladay <golee10000@hotmail.com>
2019-05-28 14:15:56 -0400

    updated argparse arguments to make similar to assignment requirements

--- dotm_search.py
+++ dotm_search.py
@@ -13,35 +13,25 @@ import zipfile
 def main():
     occur = 0
     files = 0
-   
     parser = argparse.ArgumentParser(description='Find string inside DOTM files')
     parser.add_argument('string', type=str,
                         help='search criteria')
-    parser.add_argument('--dir', action='store_true',
+    parser.add_argument("-d", '--dir', nargs="?" , const=str, default=os.getcwd(),
                         help='specifying the directory')
-    parser.add_argument('directory', type=str,
-                        help='directory')
-
     args = parser.parse_args()
-    if args.dir:
-        search_directory = args.directory
-    else:
-        search_directory = os.getcwd()
-    print(search_directory)
+    search_directory = args.dir
     for fn in iglob(search_directory+"/*.dotm"):
-        # print(fn)
+       
         document = zipfile.ZipFile(fn)
         read_doc = document.read("word/document.xml", pwd=None)
         stopper = 0
         for line in read_doc:
-            
             if args.string in line:
                 if stopper != 1:
                     occur += 1
                     stopper = 1
-                print("Match found in file " + fn + "\n" + read_doc[read_doc.index(line)-40:read_doc.index(line)+40])
+                print("Match found in file " + os.path.abspath(fn) + "\n" + read_doc[read_doc.index(line)-40:read_doc.index(line)+40])
         files+=1
-    print("dot_m files searched " + str(files)+"\n"+"dot_m matches found "+str(occur))
-
+    print("dot_m files searched: " + str(files)+"\n"+"dot_m matches found: "+str(occur))
 if __name__ == '__main__':
     main()
